314b459908b71961d2291e9125b5bd9c15bd1e9c
Deepak Kumar <deepak.kr.27797@gmail.com>
2025-08-26 20:24:10 +0530

    Added storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,14 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { storage } from "./storage/resource";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  storage,
 });
+
+const { cfnBucket } = backend.storage.resources.cfnResources;
+// Use a globally unique name (DNS-style, 3–63 chars)
+cfnBucket.bucketName = "ecnamplify-clinical-trails-studies";
